4a5ce88f5078d247ca0ad44e67c02a6529330a82
RylaD303 <alemak2002@gmail.com>
2022-05-24 14:17:39 +0300

    Added vehicles.

--- CarsOnThreeRoad/Vehicle.h
+++ CarsOnThreeRoad/Vehicle.h
@@ -5,8 +5,36 @@ private:
 	char* model;
 	unsigned int max_speed;
 public:
-	virtual bool CheckThirdClassRoad(double road_length, double hours) const;
-	virtual bool CheckSpeedRoad(double road_length, double hours) const;
-	virtual bool CheckHighway(double road_length, double hours) const;
+	virtual bool CheckThirdClassRoad(double road_length, double hours) const =0;
+	virtual bool CheckSpeedRoad(double road_length, double hours) const =0;
+	virtual bool CheckHighway(double road_length, double hours) const =0;
 };
+
+
+class Car : public Vehicle
+{
+public:
+	virtual bool CheckThirdClassRoad(double road_length, double hours) const override;
+	virtual bool CheckSpeedRoad(double road_length, double hours) const override;
+	virtual bool CheckHighway(double road_length, double hours) const override;
+};
+
+
+class Lorry : public Vehicle
+{
+public:
+	virtual bool CheckThirdClassRoad(double road_length, double hours) const override;
+	virtual bool CheckSpeedRoad(double road_length, double hours) const override;
+	virtual bool CheckHighway(double road_length, double hours) const override;
+};
+
+
+class Minibus : public Vehicle
+{
+public:
+	virtual bool CheckThirdClassRoad(double road_length, double hours) const override;
+	virtual bool CheckSpeedRoad(double road_length, double hours) const override;
+	virtual bool CheckHighway(double road_length, double hours) const override;
+};
+
 
